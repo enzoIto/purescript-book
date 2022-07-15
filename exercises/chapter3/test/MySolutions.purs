49293912b0d77120979f9f0dfff06b87fa13f10d
Enzo Ito <enzo.ito@madeiramadeira.com.br>
2022-07-15 09:58:11 -0300

    feat: adding findEntryByStreet function

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -2,4 +2,6 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter filterEntry
+
