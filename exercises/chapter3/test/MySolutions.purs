e59e12cabca0bcb8620c082c4f57e839afab1042
Enzo Ito <enzo.ito@madeiramadeira.com.br>
2022-08-03 03:07:58 -0300

    feat: adding solutions

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -6,3 +6,5 @@ findEntryByStreet :: String -> AddressBook -> Maybe Entry
 findEntryByStreet streetName = head <<< filter (_.address.street >>> eq streetName)
 
 
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook firstName lastName = 
